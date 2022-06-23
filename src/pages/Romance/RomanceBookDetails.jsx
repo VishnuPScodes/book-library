5b7fc6e0851ca971e878eab20b51e5b078a552f2
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-23 11:55:01 +0530

    Update RomanceBookDetails.jsx

--- src/pages/Romance/RomanceBookDetails.jsx
+++ src/pages/Romance/RomanceBookDetails.jsx
@@ -62,14 +62,14 @@ import { dataAddFailure, dataAddRequest, dataAddSuccess } from '../../redux/acti
                 lineHeight={1.1}
                 fontWeight={600}
                 fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
-                Automatic Watch
+                {singleData.name}
               </Heading>
-              <Text
+              {/* <Text
                 color={useColorModeValue('gray.900', 'gray.400')}
                 fontWeight={300}
                 fontSize={'2xl'}>
                 $350.00 USD
-              </Text>
+              </Text> */}
             </Box>
   
             <Stack
@@ -85,8 +85,7 @@ import { dataAddFailure, dataAddRequest, dataAddSuccess } from '../../redux/acti
                   color={useColorModeValue('gray.500', 'gray.400')}
                   fontSize={'2xl'}
                   fontWeight={'300'}>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore
+                 Ajay K Pandey
                 </Text>
                 <Text fontSize={'lg'}>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
@@ -96,86 +95,14 @@ import { dataAddFailure, dataAddRequest, dataAddSuccess } from '../../redux/acti
                 </Text>
               </VStack>
               <Box>
-                <Text
-                  fontSize={{ base: '16px', lg: '18px' }}
-                  color={useColorModeValue('yellow.500', 'yellow.300')}
-                  fontWeight={'500'}
-                  textTransform={'uppercase'}
-                  mb={'4'}>
-                  Features
-                </Text>
+               
   
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-                  <List spacing={2}>
-                    <ListItem>Chronograph</ListItem>
-                    <ListItem>Master Chronometer Certified</ListItem>{' '}
-                    <ListItem>Tachymeter</ListItem>
-                  </List>
-                  <List spacing={2}>
-                    <ListItem>Anti‑magnetic</ListItem>
-                    <ListItem>Chronometer</ListItem>
-                    <ListItem>Small seconds</ListItem>
-                  </List>
-                </SimpleGrid>
               </Box>
               <Box>
-                <Text
-                  fontSize={{ base: '16px', lg: '18px' }}
-                  color={useColorModeValue('yellow.500', 'yellow.300')}
-                  fontWeight={'500'}
-                  textTransform={'uppercase'}
-                  mb={'4'}>
-                  Product Details
-                </Text>
-  
-                <List spacing={2}>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Between lugs:
-                    </Text>{' '}
-                    20 mm
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Bracelet:
-                    </Text>{' '}
-                    leather strap
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Case:
-                    </Text>{' '}
-                    Steel
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Case diameter:
-                    </Text>{' '}
-                    42 mm
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Dial color:
-                    </Text>{' '}
-                    Black
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Crystal:
-                    </Text>{' '}
-                    Domed, scratch‑resistant sapphire crystal with anti‑reflective
-                    treatment inside
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Water resistance:
-                    </Text>{' '}
-                    5 bar (50 metres / 167 feet){' '}
-                  </ListItem>
-                </List>
+           
               </Box>
             </Stack>
-  
+            <a href={singleData.download} download>   
             <Button
               rounded={'none'}
               w={'full'}
@@ -189,12 +116,10 @@ import { dataAddFailure, dataAddRequest, dataAddSuccess } from '../../redux/acti
                 transform: 'translateY(2px)',
                 boxShadow: 'lg',
               }}>
-              Add to cart
+              Download
             </Button>
-  
-            <Stack direction="row" alignItems="center" justifyContent={'center'}>
-              <MdLocalShipping />
-              <Text>2-3 business days delivery</Text>
+            </a>
+            <Stack direction="row" alignItems="center" justifyContent={'center'}>          
             </Stack>
           </Stack>
         </SimpleGrid>
