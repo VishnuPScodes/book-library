bc6d14136ca79b95a51750cb13f70dd677428ac2
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-06-23 23:26:52 +0530

    updated

--- src/components/Allroutes.jsx
+++ src/components/Allroutes.jsx
@@ -1,4 +1,7 @@
+import { Children } from "react"
 import { Route, Routes } from "react-router-dom"
+import { ChildrenCard } from "../pages/Children/ChildrenCard"
+import { ChildrenLoader } from "../pages/Children/ChildrenLoader"
 import { Home } from "../pages/Home/Home"
 import { Romance } from "../pages/Romance/Romance"
 import { RomanceBookDetails } from "../pages/Romance/RomanceBookDetails"
@@ -14,6 +17,11 @@ export const Allroutes=(()=>{
     <Route  path="/Romance" element={<Romance/>}  />
     <Route  path="/RomanceCard" element={<RomanceCard/>}  />
     <Route  path="/RomanceLoader" element={<RomanceLoader/>}  />
+    <Route  path="/RomanceBookDetails/:id" element={<RomanceBookDetails/>}/>
+
+    <Route  path="/Children" element={<Children/>}  />
+    <Route  path="/ChildrenCard" element={<ChildrenCard/>}  />
+    <Route  path="/ChildrenLoader" element={<ChildrenLoader/>}  />
     <Route  path="/RomanceBookDetails/:id" element={<RomanceBookDetails/>}/>
     
 </Routes>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,8 @@ export const Home=(()=>{
         <Heading>Choose Your Favorite Section</Heading>
 
          <Grid id="main-grid" margin={'auto'} gap={6} templateColumns={{lg:"49% 49%" ,sm:"95%"}}>
-            <GridItem id="romance" borderRadius={'20px'} h={'400px'} onClick={handleRomance}  bg={{base:"yellow",sm:"red"}} ><Heading id="heading-romance">Romance</Heading></GridItem>
-            <GridItem  borderRadius={'20px'} h={'400px'} bg={{base:"green" ,sm:"red",md:"brown"}}><Heading id="heading-children">Children's Section</Heading></GridItem>
+            <GridItem id="romance" borderRadius={'20px'} h={'400px'}  onClick={handleRomance}  bg={{base:"yellow",sm:"red"}} ><Heading id="heading-romance">Romance</Heading></GridItem>
+            <GridItem  borderRadius={'20px'} h={'400px'} onClick={handleChildren} bg={{base:"green" ,sm:"red",md:"brown"}}><Heading id="heading-children">Children's Section</Heading></GridItem>
             <GridItem  borderRadius={'20px'} h={'400px'} bg={'orange'} >Short Stories</GridItem>
             <GridItem  borderRadius={'20px'} h={'400px'} bg={'yellow'}>Classics Novels</GridItem>
          </Grid>
