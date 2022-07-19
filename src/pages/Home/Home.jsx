461c88cd8edbd8c61c963e8d33d455c51ed40c62
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-07-20 00:35:56 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,13 +20,22 @@ export const Home=(()=>{
     const handleChildren=(()=>{
         navigate('/Children')
     })
-    return <div id="home-container-main">
+    // hover properties
+    const hoverRomance=(()=>{
+       let heading=document.getElementById('heading-main')
+       heading.innerHTML='Choose Romance'
+    })
+    const hoverMain=(()=>{
+        let heading=document.getElementById('heading-main')
+        heading.innerHTML='Choose Your Favorite Section'
+    })
+    return <div id="home-container-main">   
         <Stack   id="home-box"> 
-        <Heading>Choose Your Favorite Section</Heading>
+        <Heading id='heading-main' >Choose Your Favorite Section</Heading>
         </Stack>
          <Grid id="main-grid" margin={'auto'} gap={6} templateColumns={{lg:"49% 49%" ,sm:"95%"}}>
             
-            <GridItem id="romance" borderRadius={'20px'} h={'400px'}  onClick={handleRomance}  bg={{base:"yellow",sm:"red"}} ><Heading id="heading-romance">Romance</Heading></GridItem>
+            <GridItem id="romance" borderRadius={'20px'} h={'400px'} onMouseOut={hoverMain}  onMouseEnter={hoverRomance}  onClick={handleRomance}  bg={{base:"yellow",sm:"red"}} ><Heading id="heading-romance">Romance</Heading></GridItem>
             <GridItem  id="children" borderRadius={'20px'} h={'400px'} onClick={handleChildren} bg={{base:"green" ,sm:"red",md:"brown"}}><Heading id="heading-children">  Children's Section</Heading></GridItem>
            <GridItem id="shortstory"  borderRadius={'20px'} h={'400px'} bg={'orange'} >Short Stories</GridItem> 
             <GridItem id="classics"  borderRadius={'20px'} h={'400px'} bg={'yellow'}>Classics Novels</GridItem>
