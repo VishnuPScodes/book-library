02984555b46d04feb27aeb9503e6f69cccf23c41
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2022-07-26 12:35:41 +0530

    Delete routes directory

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-import { getPosts ,createPosts} from "../controller/posts.js";
-import express from "express";
-
-const router=express.Router();
-
-router.get('/',getPosts)
-router.post('/',createPosts)
-
-export default router
